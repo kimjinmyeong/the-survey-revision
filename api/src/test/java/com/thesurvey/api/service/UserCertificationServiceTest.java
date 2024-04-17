57f251ad0264e6732999a0aeba3dbc6c811b475a
kimjinmyeong <kjmdkdlel@gmail.com>
2024-04-18 01:34:16 +0900

    feat: active test profile

--- api/src/test/java/com/thesurvey/api/service/UserCertificationServiceTest.java
+++ api/src/test/java/com/thesurvey/api/service/UserCertificationServiceTest.java
@@ -15,12 +15,14 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 public class UserCertificationServiceTest {
 
     @Autowired
