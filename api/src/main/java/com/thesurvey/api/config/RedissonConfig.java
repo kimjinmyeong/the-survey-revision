3c2d083e272b5f8e3698b214b7323b1711dd0705
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-07-10 19:54:34 +0900

    feat: add cacheManager

--- api/src/main/java/com/thesurvey/api/config/RedissonConfig.java
+++ api/src/main/java/com/thesurvey/api/config/RedissonConfig.java
@@ -4,12 +4,21 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.redisson.spring.cache.CacheConfig;
+import org.redisson.spring.cache.RedissonSpringCacheManager;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 @EnableRedisHttpSession
+@EnableCaching
 public class RedissonConfig {
 
     @Bean
@@ -25,6 +34,17 @@ public class RedissonConfig {
 //        config.useSentinelServers()
 //                .setMasterName("mymaster")
 //                .addSentinelAddress("redis://127.0.0.1:26379", "redis://127.0.0.1:26380", "redis://127.0.0.1:26381");
+
         return Redisson.create(config);
+    }
+
+    @Bean
+    CacheManager cacheManager(RedissonClient redissonClient) {
+        Map<String, CacheConfig> config = new HashMap<>();
+        config.put("surveyListCache", new CacheConfig(
+                TimeUnit.HOURS.toMillis(1), // TTL 1 hour
+                TimeUnit.MINUTES.toMillis(30) // Max idle time 30 minutes
+        ));
+        return new RedissonSpringCacheManager(redissonClient, config);
     }
 }
