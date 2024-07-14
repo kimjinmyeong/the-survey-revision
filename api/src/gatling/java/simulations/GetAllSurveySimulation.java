cfef8a02909ac0393b56a923ca4864aac5d42546
kimjinmyeong <kjmdkdlel@gmail.com>
2024-07-14 19:28:51 +0900

    feat: add BaseSimulation class

--- api/src/gatling/java/simulations/GetAllSurveySimulation.java
+++ api/src/gatling/java/simulations/GetAllSurveySimulation.java
@@ -1,81 +1,71 @@
 package simulations;
 
 import io.gatling.javaapi.core.ScenarioBuilder;
-import io.gatling.javaapi.core.Simulation;
-import io.gatling.javaapi.http.HttpProtocolBuilder;
 
 import java.time.Duration;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
-public class GetAllSurveySimulation extends Simulation {
+public class GetAllSurveySimulation extends BaseSimulation {
 
-    HttpProtocolBuilder httpProtocol = http
-            .baseUrl("http://localhost:8080/v1")
-            .acceptHeader("application/json")
-            .contentTypeHeader("application/json")
-            .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+    private final ScenarioBuilder createSurveyScn;
+    private final ScenarioBuilder getAllSurveyScn;
 
-    Map<String, String> headers = Map.of("Content-Type", "application/json");
+    public GetAllSurveySimulation() {
+        createSurveyScn = scenario("Create Surveys")
+                .feed(feeder)
+                .exec(http("User Registration")
+                        .post("/auth/register")
+                        .headers(headers)
+                        .body(StringBody(session -> {
+                            int count = session.getInt("count");
+                            String name = "testUser" + count;
+                            String email = "testUser" + count + "@gmail.com";
+                            String password = "Password40@";
+                            String phoneNumber = "01012345678";
+                            return String.format("{\"name\":\"%s\",\"email\":\"%s\",\"password\":\"%s\",\"phoneNumber\":\"%s\"}", name, email, password, phoneNumber);
+                        })).asJson()
+                        .check(status().is(200)))
+                .pause(1)
+                .exec(http("User Login")
+                        .post("/auth/login")
+                        .headers(headers)
+                        .body(StringBody(session -> {
+                            String email = "testUser" + session.getInt("count") + "@gmail.com";
+                            String password = "Password40@";
+                            return String.format("{\"email\":\"%s\",\"password\":\"%s\"}", email, password);
+                        })).asJson()
+                        .check(status().is(200))
+                        .check(headerRegex("Set-Cookie", "JSESSIONID=(.*?);").saveAs("jsessionid")))
+                .pause(1)
+                .repeat(15, "n").on(
+                        exec(http("Create Survey #{n}")
+                                .post("/surveys")
+                                .headers(Map.of("Content-Type", "application/json"))
+                                .body(RawFileBody("data/survey_request.json")).asJson()
+                                .check(status().is(200))
+                        )
+                );
 
-    private static final AtomicInteger count = new AtomicInteger(1);
-    private static final Iterator<Map<String, Object>> feeder = Stream.generate(() -> Collections.singletonMap("count", (Object) count.getAndIncrement())).iterator();
+        getAllSurveyScn = scenario("Get All Surveys")
+                .exec(http("Get Surveys Page 1")
+                        .get("/surveys")
+                        .queryParam("page", "1")
+                        .check(jsonPath("$.surveys").exists())
+                        .headers(headers)
+                        .check(status().is(200))
+                );
+        String testType = System.getProperty("type");
+        setupSimulation(testType);
+    }
 
-    ScenarioBuilder createSurveysScn = scenario("Create Surveys")
-            .feed(feeder)
-            .exec(http("User Registration")
-                    .post("/auth/register")
-                    .headers(headers)
-                    .body(StringBody(session -> {
-                        int count = session.getInt("count");
-                        String name = "testUser" + count;
-                        String email = "testUser" + count + "@gmail.com";
-                        String password = "Password40@";
-                        String phoneNumber = "01012345678";
-                        return String.format("{\"name\":\"%s\",\"email\":\"%s\",\"password\":\"%s\",\"phoneNumber\":\"%s\"}", name, email, password, phoneNumber);
-                    })).asJson()
-                    .check(status().is(200)))
-            .pause(1)
-            .exec(http("User Login")
-                    .post("/auth/login")
-                    .headers(headers)
-                    .body(StringBody(session -> {
-                        String email = "testUser" + session.getInt("count") + "@gmail.com";
-                        String password = "Password40@";
-                        return String.format("{\"email\":\"%s\",\"password\":\"%s\"}", email, password);
-                    })).asJson()
-                    .check(status().is(200))
-                    .check(headerRegex("Set-Cookie", "JSESSIONID=(.*?);").saveAs("jsessionid")))
-            .pause(1)
-            .repeat(15, "n").on(
-                    exec(http("Create Survey #{n}")
-                            .post("/surveys")
-                            .headers(headers)
-                            .body(RawFileBody("data/survey_request.json")).asJson()
-                            .check(status().is(200))
-                    )
-            );
-
-    ScenarioBuilder getAllSurveysScn = scenario("Get All Surveys")
-            .exec(http("Get Surveys Page 1")
-                    .get("/surveys")
-                    .queryParam("page", "1")
-                    .check(jsonPath("$.surveys").exists())
-                    .headers(headers)
-                    .check(status().is(200))
-            );
-
-    // Load Testing Setup
+    @Override
     public void loadTestingSetup() {
         setUp(
-                createSurveysScn.injectOpen(atOnceUsers(1)),
-                getAllSurveysScn.injectOpen(
+                createSurveyScn.injectOpen(atOnceUsers(1)),
+                getAllSurveyScn.injectOpen(
                         nothingFor(5),
                         atOnceUsers(1000),
                         rampUsers(5000).during(Duration.ofSeconds(300))
@@ -83,47 +73,26 @@ public class GetAllSurveySimulation extends Simulation {
         ).protocols(httpProtocol);
     }
 
-    // Stress Testing Setup
+    @Override
     public void stressTestingSetup() {
         setUp(
-                createSurveysScn.injectOpen(atOnceUsers(1)),
-                getAllSurveysScn.injectOpen(
+                createSurveyScn.injectOpen(atOnceUsers(1)),
+                getAllSurveyScn.injectOpen(
                         nothingFor(5),
                         rampUsers(10000).during(Duration.ofSeconds(600))
                 )
         ).protocols(httpProtocol);
     }
 
-    // Soak Testing Setup
+    @Override
     public void soakTestingSetup() {
         setUp(
-                createSurveysScn.injectOpen(atOnceUsers(1)),
-                getAllSurveysScn.injectOpen(
+                createSurveyScn.injectOpen(atOnceUsers(1)),
+                getAllSurveyScn.injectOpen(
                         nothingFor(5),
                         constantUsersPerSec(50).during(Duration.ofMinutes(30))
                 )
         ).protocols(httpProtocol);
     }
 
-    // Main Simulation Setup
-    {
-        String testType = System.getProperty("type");
-        if (testType == null) {
-            testType = "LOAD"; // Default to LOAD if no system property is set
-        }
-
-        switch (testType.toUpperCase()) {
-            case "LOAD":
-                loadTestingSetup();
-                break;
-            case "STRESS":
-                stressTestingSetup();
-                break;
-            case "SOAK":
-                soakTestingSetup();
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown type: " + testType);
-        }
-    }
 }
--- api/src/gatling/java/simulations/SurveySubmitAnswerSimulation.java
+++ api/src/gatling/java/simulations/SurveySubmitAnswerSimulation.java
@@ -1,39 +1,22 @@
 package simulations;
 
 import io.gatling.javaapi.core.ScenarioBuilder;
-import io.gatling.javaapi.core.Simulation;
-import io.gatling.javaapi.http.HttpProtocolBuilder;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
-public class SurveySubmitAnswerSimulation extends Simulation {
+public class SurveySubmitAnswerSimulation extends BaseSimulation {
 
-        HttpProtocolBuilder httpProtocol = http
-                .baseUrl("http://localhost:8080/v1") // Base URL of your Spring Boot application
-                .acceptHeader("application/json")
-                .contentTypeHeader("application/json")
-                .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+    private final ScenarioBuilder createSurveyScn;
+    private final ScenarioBuilder submitSurveyScn;
 
-        Map<String, String> headers = Map.of("Content-Type", "application/json");
-
-        // AtomicInteger to ensure unique counts
-        private static final AtomicInteger count = new AtomicInteger(1);
-
-        // Iterator to provide unique count values
-        private static final Iterator<Map<String, Object>> feeder = Stream.generate(() -> Collections.singletonMap("count", (Object) count.getAndIncrement())).iterator();
-
-        // Scenario to create a survey
-        ScenarioBuilder createSurveysScn = scenario("Survey Create Scenario")
+    public SurveySubmitAnswerSimulation() {
+        createSurveyScn = scenario("Survey Create Scenario")
                 .exec(http("User Registration")
                         .post("/auth/register")
                         .headers(headers)
@@ -77,44 +60,47 @@ public class SurveySubmitAnswerSimulation extends Simulation {
                         .check(status().is(200))
                         .check(jsonPath("$.surveyId").saveAs("surveyId")));
 
-    // Scenario to create a survey
-    ScenarioBuilder submitSurveysScn = scenario("Submit Survey Scenario")
-            .feed(feeder)
-            .exec(http("User Registration")
-                    .post("/auth/register")
-                    .headers(headers)
-                    .body(StringBody(session -> {
-                        int count = session.getInt("count");
-                        String name = "testUser" + count;
-                        String email = "testUser" + count + "@gmail.com";
-                        String password = "Password40@";
-                        String phoneNumber = "01012345678";
-                        return String.format("{\"name\":\"%s\",\"email\":\"%s\",\"password\":\"%s\",\"phoneNumber\":\"%s\"}", name, email, password, phoneNumber);
-                    })).asJson()
-                    .check(status().is(200)))
-            .pause(1)
-            .exec(http("User Login")
-                    .post("/auth/login")
-                    .headers(headers)
-                    .body(StringBody(session -> {
-                        String email = "testUser" + session.getInt("count") + "@gmail.com";
-                        String password = "Password40@";
-                        return String.format("{\"email\":\"%s\",\"password\":\"%s\"}", email, password);
-                    })).asJson()
-                    .check(status().is(200))
-                    .check(headerRegex("Set-Cookie", "JSESSIONID=(.*?);").saveAs("jsessionid")))
-            .pause(1)
-            .exec(http("Submit Survey")
-                    .post("/surveys/submit")
-                    .headers(Map.of("Content-Type", "application/json", "Cookie", "JSESSIONID=${jsessionid}"))
-                    .body(RawFileBody("data/submit_request.json")).asJson()
-                    .check(status().is(200))
-            );
+        submitSurveyScn = scenario("Submit Survey Scenario")
+                .feed(feeder)
+                .exec(http("User Registration")
+                        .post("/auth/register")
+                        .headers(headers)
+                        .body(StringBody(session -> {
+                            int count = session.getInt("count");
+                            String name = "testUser" + count;
+                            String email = "testUser" + count + "@gmail.com";
+                            String password = "Password40@";
+                            String phoneNumber = "01012345678";
+                            return String.format("{\"name\":\"%s\",\"email\":\"%s\",\"password\":\"%s\",\"phoneNumber\":\"%s\"}", name, email, password, phoneNumber);
+                        })).asJson()
+                        .check(status().is(200)))
+                .pause(1)
+                .exec(http("User Login")
+                        .post("/auth/login")
+                        .headers(headers)
+                        .body(StringBody(session -> {
+                            String email = "testUser" + session.getInt("count") + "@gmail.com";
+                            String password = "Password40@";
+                            return String.format("{\"email\":\"%s\",\"password\":\"%s\"}", email, password);
+                        })).asJson()
+                        .check(status().is(200))
+                        .check(headerRegex("Set-Cookie", "JSESSIONID=(.*?);").saveAs("jsessionid")))
+                .pause(1)
+                .exec(http("Submit Survey")
+                        .post("/surveys/submit")
+                        .headers(Map.of("Content-Type", "application/json", "Cookie", "JSESSIONID=${jsessionid}"))
+                        .body(RawFileBody("data/submit_request.json")).asJson()
+                        .check(status().is(200))
+                );
+        String testType = System.getProperty("type");
+        setupSimulation(testType);
+    }
 
+    @Override
     public void loadTestingSetup() {
         setUp(
-                createSurveysScn.injectOpen(atOnceUsers(1)),
-                submitSurveysScn.injectOpen(
+                createSurveyScn.injectOpen(atOnceUsers(1)),
+                submitSurveyScn.injectOpen(
                         nothingFor(5),
                         atOnceUsers(1000),
                         rampUsers(5000).during(Duration.ofSeconds(300))
@@ -122,45 +108,26 @@ public class SurveySubmitAnswerSimulation extends Simulation {
         ).protocols(httpProtocol);
     }
 
+    @Override
     public void stressTestingSetup() {
         setUp(
-                createSurveysScn.injectOpen(atOnceUsers(1)),
-                submitSurveysScn.injectOpen(
+                createSurveyScn.injectOpen(atOnceUsers(1)),
+                submitSurveyScn.injectOpen(
                         nothingFor(5),
                         rampUsers(10000).during(Duration.ofSeconds(600))
                 )
         ).protocols(httpProtocol);
     }
 
+    @Override
     public void soakTestingSetup() {
         setUp(
-                createSurveysScn.injectOpen(atOnceUsers(1)),
-                submitSurveysScn.injectOpen(
+                createSurveyScn.injectOpen(atOnceUsers(1)),
+                submitSurveyScn.injectOpen(
                         nothingFor(5),
                         constantUsersPerSec(50).during(Duration.ofMinutes(30))
                 )
         ).protocols(httpProtocol);
-    }
-
-    {
-        String testType = System.getProperty("type");
-        if (testType == null) {
-            testType = "LOAD";
-        }
-
-        switch (testType.toUpperCase()) {
-            case "LOAD":
-                loadTestingSetup();
-                break;
-            case "STRESS":
-                stressTestingSetup();
-                break;
-            case "SOAK":
-                soakTestingSetup();
-                break;
-            default:
-                throw new IllegalArgumentException("Unknown type: " + testType);
-        }
     }
 
 }
