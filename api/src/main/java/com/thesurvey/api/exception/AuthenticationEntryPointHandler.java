477cdf284ad348b33b834550289b7d62ea8fcf5e
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-07-19 22:01:28 +0900

    refactor: remove useless "charset=UTF-8"

--- api/src/main/java/com/thesurvey/api/exception/AuthenticationEntryPointHandler.java
+++ api/src/main/java/com/thesurvey/api/exception/AuthenticationEntryPointHandler.java
@@ -1,14 +1,13 @@
 package com.thesurvey.api.exception;
 
-import java.io.IOException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import java.io.IOException;
 
 /**
  * Handles authentication errors that occur during the Spring Security filter chain, such as when a
@@ -19,9 +18,9 @@ public class AuthenticationEntryPointHandler implements AuthenticationEntryPoint
     // method is called by the Spring Security filter chain when an authentication error occurs.
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
-        AuthenticationException authException) throws IOException, ServletException {
+        AuthenticationException authException) throws IOException {
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        response.setContentType("text/plain;charset=UTF-8");
+        response.setContentType("text/plain");
         response.setCharacterEncoding("UTF-8");
         response.getWriter().write("권한이 없습니다.");
     }
